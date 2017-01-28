1e98dc7b87d04cb29f66524d57a8488530f85fa3
connie0701 <connie0701@hotmail.com>
2017-01-28 18:01:37 -0500

    Update Plot5.R

--- Plot5.R
+++ Plot5.R
@@ -7,17 +7,18 @@ b<-readRDS("C:/Users/conni/Documents/Source_Classification_Code.rds")
 
 a$year <- factor(a$year, levels=c('1999', '2002', '2005', '2008'))
 
-BC.OR <- subset(a, fips == 24510 & type == 'ON-ROAD')
+BC.OR <- subset(a, fips == '24510' & type == 'ON-ROAD')
 
-BC <- aggregate(Emissions ~ year, BC.OR, sum)
+BC <- aggregate(BC.OR[, 'Emissions'], by=list(BC.OR$year), sum)
+colnames(BC) <- c('year', 'Emissions')
 
 
 #Generate graph
 png(filename='C:/Users/conni/Documents/plot5.png')
 
-ggplot(data=BC, aes(x=year, y=Emissions)) + geom_bar(stat="identity") + 
-      ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore')+
-      xlab('year')+ylab(expression('Total PM'[2.5]) 
-                          
-
+ggplot(data=BC, aes(x=year, y=Emissions)) + geom_bar(stat="identity") + guides(fill=F) + 
+  ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore') + 
+  ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none')
+                    
+                    
 dev.off()
